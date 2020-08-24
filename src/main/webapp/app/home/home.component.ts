b472f27a2dbe69e5dbfc71f3b73388c0c8f21afa
thiva.poc-at-805694229312 <thivahar.kugabrasatham@gmail.com>
2020-08-24 23:21:42 +0800

    Auto login user

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -4,6 +4,9 @@ import { Subscription } from 'rxjs';
 import { LoginModalService } from 'app/core/login/login-modal.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/user/account.model';
+import {AuthServerProvider} from "app/core/auth/auth-jwt.service";
+import {LoginService} from "app/core/login/login.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'jhi-home',
@@ -13,10 +16,39 @@ import { Account } from 'app/core/user/account.model';
 export class HomeComponent implements OnInit, OnDestroy {
   account: Account | null = null;
   authSubscription?: Subscription;
+  displayName: String = "";
 
-  constructor(private accountService: AccountService, private loginModalService: LoginModalService) {}
+  authenticationError = false;
+
+  constructor(private accountService: AccountService, private loginModalService: LoginModalService,
+              private authServerProvider: AuthServerProvider,
+              private loginService: LoginService, private router: Router) {}
 
   ngOnInit(): void {
+    if(!this.accountService.isAuthenticated()) {
+      // auto login
+      this.loginService
+        .login({
+          username: "user",
+          password: "Password1#",
+          rememberMe: false,
+        })
+        .subscribe(
+          () => {
+            if (
+              this.router.url === '/account/register' ||
+              this.router.url.startsWith('/account/activate') ||
+              this.router.url.startsWith('/account/reset/')
+            ) {
+              this.router.navigate(['']);
+            }
+            this.displayName = "My User";
+          },
+          () => {
+            this.authenticationError = true
+          }
+        );
+    }
     this.authSubscription = this.accountService.getAuthenticationState().subscribe(account => (this.account = account));
   }
 
