d25805570047a4c3499e49f9c7c16f6965861a37
thiva.poc-at-805694229312 <thivahar.kugabrasatham@gmail.com>
2020-08-25 06:39:00 +0800

    Auto login user

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/user/account.model';
 import {AuthServerProvider} from "app/core/auth/auth-jwt.service";
 import {LoginService} from "app/core/login/login.service";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'jhi-home',
@@ -20,34 +20,46 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   authenticationError = false;
 
+  autoLogin = "no";
+
   constructor(private accountService: AccountService, private loginModalService: LoginModalService,
               private authServerProvider: AuthServerProvider,
-              private loginService: LoginService, private router: Router) {}
+              private loginService: LoginService, private router: Router,
+              private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     if(!this.accountService.isAuthenticated()) {
+      this.activatedRoute.queryParams.subscribe(params => {
+        const userName = params['userName'];
+        this.autoLogin = params['auto_login'];
+        if(userName != null) {
+          this.displayName = userName;
+        }
+      });
       // auto login
-      this.loginService
-        .login({
-          username: "user",
-          password: "Password1#",
-          rememberMe: false,
-        })
-        .subscribe(
-          () => {
-            if (
-              this.router.url === '/account/register' ||
-              this.router.url.startsWith('/account/activate') ||
-              this.router.url.startsWith('/account/reset/')
-            ) {
-              this.router.navigate(['']);
+      if(this.autoLogin === "true") {
+        this.loginService
+          .login({
+            username: "user",
+            password: "Password1#",
+            rememberMe: false,
+          })
+          .subscribe(
+            () => {
+              if (
+                this.router.url === '/account/register' ||
+                this.router.url.startsWith('/account/activate') ||
+                this.router.url.startsWith('/account/reset/')
+              ) {
+                this.router.navigate(['']);
+              }
+            },
+            () => {
+              this.authenticationError = true
             }
-            this.displayName = "My User";
-          },
-          () => {
-            this.authenticationError = true
-          }
-        );
+          );
+      }
+
     }
     this.authSubscription = this.accountService.getAuthenticationState().subscribe(account => (this.account = account));
   }
